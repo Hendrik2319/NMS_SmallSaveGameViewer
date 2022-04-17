674a6bfd9a9d913b4bb70ea79ab6f4d92b5905f4
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2022-04-17 11:47:53 +0000

    JAVA: [Eclipse] - Projekt "NMS_SmallSaveGameViewer"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/NMS_SmallSaveGameViewer@6339 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/games/nomanssky/smallsavegameviewer/NMS_SmallSaveGameViewer.java
+++ src/net/schwarzbaer/java/games/nomanssky/smallsavegameviewer/NMS_SmallSaveGameViewer.java
@@ -5,11 +5,20 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Vector;
 import java.util.function.Supplier;
@@ -23,6 +32,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.TableCellRenderer;
@@ -35,7 +45,9 @@ import net.schwarzbaer.system.Settings;
 
 public class NMS_SmallSaveGameViewer {
 	
+	private static final String FILENAME_SLOT_DESCRIPTIONS = "NMS_SmallSaveGameViewer.SlotDescriptions.txt";
 	private static final AppSettings settings = new AppSettings();
+	private static final HashMap<Integer, String> slotDescriptions = new HashMap<Integer,String>();
 	
 	public static void main(String[] args) {
 		try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
@@ -110,10 +122,60 @@ public class NMS_SmallSaveGameViewer {
 
 
 	private void initialize() {
+		readSlotDescriptions();
 		scanFolder();
 	}
 
+	private static String getLineValue(String line, String prefix) {
+		if (line.startsWith(prefix))
+			return line.substring(prefix.length());
+		return null;
+	}
 
+	private static void readSlotDescriptions() {
+		slotDescriptions.clear();
+		try (BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(FILENAME_SLOT_DESCRIPTIONS), StandardCharsets.UTF_8))) {
+			
+			String line, valueStr;
+			while ( (line=in.readLine())!=null ) {
+				
+				if ( (valueStr=getLineValue(line, "Slot|"))!=null ) {
+					int pos = valueStr.indexOf('|');
+					String numberStr   = pos<0 ? "" : valueStr.substring(0, pos);
+					String description = pos<0 ? "" : valueStr.substring(pos+1);
+					try { slotDescriptions.put(Integer.parseInt(numberStr), description); }
+					catch (NumberFormatException e) {}
+				}
+				
+			}
+			
+		}
+		catch (FileNotFoundException e) {
+			//e.printStackTrace();
+		} catch (IOException e) {
+			System.err.printf("IOException while reading SlotDescriptions: %s%n", e.getMessage());
+			// e.printStackTrace();
+		}
+	}
+
+	private static void writeSlotDescriptions() {
+		try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(FILENAME_SLOT_DESCRIPTIONS), StandardCharsets.UTF_8))) {
+			
+			Vector<Integer> slotNumbers = new Vector<>(slotDescriptions.keySet());
+			slotNumbers.sort(null);
+			for (Integer slot : slotNumbers) {
+				if (slot==null) continue;
+				String description = slotDescriptions.get(slot);
+				if (!description.isEmpty())
+					out.printf("Slot|%d|%s%n", slot, description);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.err.printf("FileNotFoundException while writing SlotDescriptions: %s%n", e.getMessage());
+			// e.printStackTrace();
+		}
+	}
+	
 	private void scanFolder() {
 		File saveGamesFolder = settings.getFile(ValueKey.SaveGamesFolder, null);
 		System.out.printf("SaveGamesFolder: %s%n", saveGamesFolder);
@@ -141,7 +203,7 @@ public class NMS_SmallSaveGameViewer {
 			saveGame.scanFile();
 			
 			saveGames.forEach(sg->{
-				if (sg.groupNumber==saveGame.groupNumber) {
+				if (sg.slotNumber==saveGame.slotNumber) {
 					sg.setGroupSibling(saveGame);
 					saveGame.setGroupSibling(sg);
 				}
@@ -151,7 +213,7 @@ public class NMS_SmallSaveGameViewer {
 		}
 		System.out.printf("%d SaveGames found%n", saveGames.size());
 		
-		saveGames.sort(Comparator.<SaveGame,Integer>comparing(sg->sg.number).thenComparing(sg->sg.file.getName()));
+		saveGames.sort(Comparator.<SaveGame,Integer>comparing(sg->sg.fileNumber).thenComparing(sg->sg.file.getName()));
 		
 		tableModel.setData(saveGames);
 	}
@@ -177,8 +239,8 @@ public class NMS_SmallSaveGameViewer {
 		enum Type { Automatic, Manual }
 		
 		private final File file;
-		private final int number;
-		private final int groupNumber;
+		private final int fileNumber;
+		private final int slotNumber;
 		private final Type type;
 		
 		private boolean isNewFormat = false;
@@ -189,17 +251,17 @@ public class NMS_SmallSaveGameViewer {
 		private Integer units = null;
 		private Integer nanites = null;
 		private Integer quicksilver = null;
-		private SaveGame groupSibling = null;
+		private SaveGame slotSibling = null;
 		
-		SaveGame(File file, int number) {
+		SaveGame(File file, int fileNumber) {
 			this.file = file;
-			this.number = number;
-			type = (this.number & 0x1)==0 ? Type.Manual : Type.Automatic;
-			groupNumber = (this.number+1) >> 1;
+			this.fileNumber = fileNumber;
+			type = (this.fileNumber & 0x1)==0 ? Type.Manual : Type.Automatic;
+			slotNumber = (this.fileNumber+1) >> 1;
 		}
 
 		void setGroupSibling(SaveGame groupSibling) {
-			this.groupSibling = groupSibling;
+			this.slotSibling = groupSibling;
 		}
 
 		void scanFile() {
@@ -306,22 +368,22 @@ public class NMS_SmallSaveGameViewer {
 		}
 
 		boolean areDatesOkWithSibling() {
-			if (groupSibling==null) return true;
+			if (slotSibling==null) return true;
 			
-			boolean isOlder = file.lastModified()<groupSibling.file.lastModified();
-			if (timeAlive    !=null && groupSibling.timeAlive    !=null && isOlder!=(timeAlive    <groupSibling.timeAlive    )) return false;
-			if (totalPlayTime!=null && groupSibling.totalPlayTime!=null && isOlder!=(totalPlayTime<groupSibling.totalPlayTime)) return false;
+			boolean isOlder = file.lastModified()<slotSibling.file.lastModified();
+			if (timeAlive    !=null && slotSibling.timeAlive    !=null && isOlder!=(timeAlive    <slotSibling.timeAlive    )) return false;
+			if (totalPlayTime!=null && slotSibling.totalPlayTime!=null && isOlder!=(totalPlayTime<slotSibling.totalPlayTime)) return false;
 			return true;
 		}
 
 		Boolean isOlderThanSibling(SaveGameTableModel.ColumnID columnID) {
 			if (columnID==null) return null;
-			if (groupSibling==null) return null;
+			if (slotSibling==null) return null;
 			
 			switch (columnID) {
-			case FileDate     : return file.lastModified()<groupSibling.file.lastModified();
-			case TimeAlive    : return timeAlive    ==null || groupSibling.timeAlive    ==null ? null : timeAlive<groupSibling.timeAlive;
-			case TotalPlayTime: return totalPlayTime==null || groupSibling.totalPlayTime==null ? null : totalPlayTime<groupSibling.totalPlayTime;
+			case FileDate     : return file.lastModified()<slotSibling.file.lastModified();
+			case TimeAlive    : return timeAlive    ==null || slotSibling.timeAlive    ==null ? null : timeAlive<slotSibling.timeAlive;
+			case TotalPlayTime: return totalPlayTime==null || slotSibling.totalPlayTime==null ? null : totalPlayTime<slotSibling.totalPlayTime;
 			default           : return null;
 			}
 		}
@@ -329,13 +391,23 @@ public class NMS_SmallSaveGameViewer {
 	}
 
 	private static class SaveGameTableModel extends Tables.SimplifiedTableModel<SaveGameTableModel.ColumnID> {
-
+		
+		private enum Alignment {
+			Left  (SwingConstants.LEFT  ),
+			Right (SwingConstants.RIGHT ),
+			Center(SwingConstants.CENTER),
+			;
+			private final int constant;
+			Alignment(int constant) { this.constant = constant; }
+		}
+		
 		private enum ColumnID implements Tables.SimplifiedColumnIDInterface {
-			Number       ("No"           ,       Integer.class,  25),
-			Name         ("Name"         ,        String.class,  70),
+			FileNumber   ("#"            ,       Integer.class,  25, Alignment.Center),
+			File         ("File"         ,        String.class,  70),
 			FileDate     ("Date"         ,        String.class, 110),
-			Group        ("Group"        ,       Integer.class,  40),
-			Type         ("Type"         , SaveGame.Type.class,  65),
+			Slot         ("Slot"         ,       Integer.class,  30, Alignment.Center),
+			Description  ("Description"  ,        String.class, 200, Alignment.Left),
+			Type         ("Type"         , SaveGame.Type.class,  65, Alignment.Center),
 			Header       ("Header"       ,        String.class,  80),
 			NewFormat    ("New Format"   ,       Boolean.class,  75),
 			Version      ("Version"      ,       Integer.class,  50),
@@ -346,7 +418,12 @@ public class NMS_SmallSaveGameViewer {
 			Quicksilver  ("Quicksilver"  ,        String.class,  70),
 			;
 			private final Tables.SimplifiedColumnConfig config;
+			private final Alignment alignment;
 			ColumnID(String name, Class<?> columnClass, int width) {
+				this(name, columnClass, width, Alignment.Right);
+			}
+			ColumnID(String name, Class<?> columnClass, int width, Alignment alignment) {
+				this.alignment = alignment;
 				config = new Tables.SimplifiedColumnConfig(name, columnClass, 20, -1, width, width);
 			}
 			@Override public Tables.SimplifiedColumnConfig getColumnConfig() { return config; }
@@ -361,7 +438,10 @@ public class NMS_SmallSaveGameViewer {
 		}
 		
 		void setRenderers() {
-			table.setDefaultRenderer(String.class, new RightStringRenderer());
+			RightStringRenderer renderer = new RightStringRenderer();
+			table.setDefaultRenderer(Integer.class, renderer);
+			table.setDefaultRenderer(String.class, renderer);
+			table.setDefaultRenderer(SaveGame.Type.class, renderer);
 		}
 		
 		private class RightStringRenderer implements TableCellRenderer {
@@ -390,9 +470,10 @@ public class NMS_SmallSaveGameViewer {
 					}
 				}
 				
-				rendererComponent.configureAsTableCellRendererComponent(table, null, value==null ? null : value.toString(), isSelected, hasFocus, getCustomBackground, null);
-				rendererComponent.setHorizontalAlignment(SwingConstants.RIGHT);
-				
+				int alignment = columnID==null ? SwingConstants.RIGHT : columnID.alignment.constant;
+				String valueStr = value==null ? null : value.toString();
+				rendererComponent.configureAsTableCellRendererComponent(table, null, valueStr, isSelected, hasFocus, getCustomBackground, null);
+				rendererComponent.setHorizontalAlignment(alignment);
 				
 				return rendererComponent;
 			}
@@ -421,10 +502,11 @@ public class NMS_SmallSaveGameViewer {
 			if (row==null) return null;
 			
 			switch (columnID) {
-			case Number       : return row.number;
-			case Name         : return row.file.getName();
+			case FileNumber   : return row.fileNumber;
+			case File         : return row.file.getName();
 			case FileDate     : return toDateStr(row.file.lastModified());
-			case Group        : return row.groupNumber;
+			case Slot         : return row.slotNumber;
+			case Description  : return slotDescriptions.get(row.slotNumber);
 			case Type         : return row.type;
 			case Header       : return String.format("0x%08X", row.header);
 			case NewFormat    : return row.isNewFormat;
@@ -439,11 +521,38 @@ public class NMS_SmallSaveGameViewer {
 			return null;
 		}
 
-		private String toDateStr(long time_ms) {
+		@Override
+		protected boolean isCellEditable(int rowIndex, int columnIndex, ColumnID columnID) {
+			return columnID==ColumnID.Description;
+		}
+
+		@Override
+		protected void setValueAt(Object aValue, int rowIndex, int columnIndex, ColumnID columnID) {
+			SaveGame row = getRow(rowIndex);
+			if (row==null) return;
+			
+			switch (columnID) {
+			
+			case Description:
+				if (aValue instanceof String) slotDescriptions.put(row.slotNumber, (String) aValue);
+				else if (aValue==null)        slotDescriptions.remove(row.slotNumber);
+				SwingUtilities.invokeLater(()->{
+					fireTableColumnUpdate(ColumnID.Description);
+					writeSlotDescriptions();
+				});
+				break;
+				
+			default:
+				break;
+				
+			}
+		}
+
+		private static String toDateStr(long time_ms) {
 			return dateTimeFormatter.getTimeStr(time_ms, false, true, false, true, false);
 		}
 
-		private String toTimeStr(Integer time_s) {
+		private static String toTimeStr(Integer time_s) {
 			if (time_s==null) return null;
 			return DateTimeFormatter.getDurationStr(time_s.longValue());
 		}
